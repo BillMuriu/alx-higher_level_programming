f43ff8ba42bf84fa3f40f76c8a55799837bc5b87
Bill <njorogekamau63@gmail.com>
2022-09-10 02:58:00 -0700

    Objects and Closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,4 +5,4 @@ const B = process.argv[3];
 const C = process.argv[4];
 const tA = fs.readFileSync(A, 'utf8');
 const tB = fs.readFileSync(B, 'utf-8');
-fs.writeFileSync(C, tA + tB);
+fs.writeFileSync(C,tA + tB);
